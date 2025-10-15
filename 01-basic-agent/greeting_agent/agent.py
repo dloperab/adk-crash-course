f73353e119efd47e0e4dc95ab71763c5f8ecaa33
David Lopera <davidlopera@gmail.com>
2025-10-14 21:53:36 -0500

    Project structure + Basic agent

---
+++ 01-basic-agent/greeting_agent/agent.py
@@ -0,0 +1,11 @@
+from google.adk.agents.llm_agent import Agent
+
+root_agent = Agent(
+    name="greeting_agent",
+    model="gemini-2.0-flash",
+    description="Greeting Agent",
+    instruction="""
+    You are a helpful assistant that greets user.
+    Ask for the user's name and greet them by name.
+    """
+)
